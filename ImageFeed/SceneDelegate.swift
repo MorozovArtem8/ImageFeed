5c785a1bbcf97b52434071de2ccd7ed5834765c5
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-05-04 15:37:40 +0300

    delete comments

--- ImageFeed/SceneDelegate.swift
+++ ImageFeed/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  ImageFeed
-//
-//  Created by Artem Morozov on 04.05.2024.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Artem Morozov on 04.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
