d0da2812d17d822607a847e17709f15be8cea0aa
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-05-07 21:31:41 +0300

    changing cell height logics

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -64,11 +64,12 @@ extension ImagesListViewController: UITableViewDelegate {
             return 0
         }
         
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
-        let scale = imageViewWidth / imageWidth
-        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
-        return cellHeight
+        let imageViewConstraints = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageViewConstraints.left - imageViewConstraints.right
+        let widthRatio = imageViewWidth / image.size.width
+        let imageViewHeight = widthRatio * image.size.height + 8
+        
+        return imageViewHeight
+        
     }
 }
