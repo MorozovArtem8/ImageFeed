58c8f3e793fdeb1f1bbf23d2ac6d1335dcdc5e34
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-06-13 20:16:35 +0300

    import ProgressHUD SPM, add loader

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 protocol AuthViewControllerDelegate: AnyObject {
     func didAuthenticate(_ vc: AuthViewController)
@@ -31,9 +32,13 @@ final class AuthViewController: UIViewController {
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         vc.dismiss(animated: true)
+        ProgressHUD.animate()
         
         OAuth2Service.shared.fetchOAuthToken(code: code) { [weak self] result in
             guard let self = self else { return }
+            
+            ProgressHUD.dismiss()
+            
             switch result {
             case .success(let result):
                 self.tokenStorage?.setToken(token: result) // Сохраняем токен
